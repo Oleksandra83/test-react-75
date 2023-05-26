89d205d1ec31a3a36b13b5305634de7fc9397c92
Oleksandra Andrushchenko <oleksandraandrusenko6@gmail.com>
2023-05-26 20:08:52 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from 'components/App';
-
+import { ThemeProvider } from '@emotion/react';
+import { App } from 'components/App/App';
+import { theme } from './constans';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
